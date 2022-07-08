bc35cc73b7b62b89e3edca2c331345849e31ae92
Cody Fulford <cody@codyfulford.com>
2022-07-07 18:48:06 -0600

    UPdate PID values after running autotune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -655,9 +655,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  27.02
+    #define DEFAULT_Ki   2.11
+    #define DEFAULT_Kd  86.48
   #endif
 #endif
 
