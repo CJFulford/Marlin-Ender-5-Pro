7e3f915b1881736d63afb80fda8845a006a35342
Cody Fulford <cody@codyfulford.com>
2022-08-16 18:32:16 -0600

    Update printer name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -146,7 +146,7 @@
 
 // Name displayed in the LCD "Ready" message and Info menu
 // ANCHOR MACHINE_NAME
-#define CUSTOM_MACHINE_NAME "E5P - CFW: 1.11"
+#define CUSTOM_MACHINE_NAME "Ender 5 Pro"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
